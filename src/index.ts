03ea51e8b0785addf7e1dff88072ab3ae21b826a
Fairuz Ikbar Wikantyo <fairuz.ikbar.w@gmail.com>
2022-08-24 17:33:02 +0700

    add hero attack daud vs fahlan

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,8 @@
-//Udah beda dari yg atas
-import { Car } from "./object/car";
-import { CarType } from "./object/car-type";
-import { Vehicle } from "./object/vehicle";
+import { Hero } from "./model/hero";
 
-const civic: Vehicle = {
-    id: '1',
-    name: 'Civic',
-    brand: 'Honda',
-    type: CarType.SEDAN,
-    engine: 2
-}
+const daud: Hero = new Hero('Daud', 100, 100, 10);
+const fahlan: Hero = new Hero('Fahlan', 100, 100, 20);
 
-const sedan: Car = new Car(civic);
-function printCar(): void {
-    const safety: string[] = ['Air Bag', 'Camera', 'Auto Pilot'];
-    const speed = sedan.speed(100);
-    const engineType = sedan.engineType('Matic');
-    const fuelType = sedan.fuelType('Gasoline');
-    const engineSpec = sedan.engineSpec(4000);
-    
-    sedan.safety(safety);
-    console.log(sedan);
-    console.log(speed, '\n', engineType, '\n', fuelType, '\n', engineSpec);
-}
-
-printCar()
-// sedan.setFuel(100);
-// sedan.startEngine();
+daud.attack(fahlan);
+console.log('Fahlan:', fahlan.toString());
+console.log('Daud:', daud.toString());
