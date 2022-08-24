6d37ca75fec318c815d39def2ab96bcf77eb9f1b
Fairuz Ikbar Wikantyo <fairuz.ikbar.w@gmail.com>
2022-08-24 16:18:12 +0700

    Jenis mobil

--- src/index.ts
+++ src/index.ts
@@ -1,137 +1,17 @@
-// TypeData
-let fullName: string = 'A';
-const age: number = 20;
-let price: number = 20_000_000;
-//underscore ini digunakan untuk membantu membaca aja. Hasil di js akan tanpa underscore
-const isDone: boolean = true;
-
-let apaAja: any = 20;
-apaAja = '20';
-apaAja = true;
-
-// null
-const isNull: null = null;
-// unknown ini bagian dari any, bisa digunakan untuk tipe data apapun. Katanya lebih aman
-const isUnknown: unknown = 'test';
-console.log(isUnknown);
-
-// any vs unknown
-let thisAny: any;
-let thisUnknown: unknown;
-
-thisAny = 'A';
-thisUnknown = 'A';
-thisAny = [];
-thisUnknown = [];
-thisAny = {};
-// thisAny.foo.bar;
-thisUnknown = {};
-// thisUnknown.foo.bar;
-// thisAny();
-// thisUnknown();
-
-let say; //undefined
-let sayAgain: undefined;
-console.log(typeof say); //undefined
-console.log(typeof sayAgain); //undefined
-
-if(sayAgain === undefined){
-    console.log('undefined');
-    
-}
-
-// Enum
-// (1) harus PascalCase;
-// (2) nilai default enum dimulai dari 0
-enum Color {
-    Red = 'Red', //nilai default nya Red = 0, kalo udah diinisiasi, nilai nya jadi berubah
-    Green = 'Green', //nilai default nya Green = 1
-    Blue = 'Blue' //nilai default nya Blue = 2
-}
-// karena Color ini jadi tipe data, maka bisa begini
-const color: Color = Color.Red;
-// const color01: Color = true; //ini akan error karena nilai true tidak ada di dalam enum Color
-console.log(Color.Red); //kalo ngga diinisiasi, outputnya 0, kalo udah outputnya Red (sesuai inisiasi)
-
-let isMarried: string | undefined;
-isMarried = 'yes';
-isMarried = undefined;
-
-// Array
-let cars: string[] = ['Avanza', 'Ferrari'];
-//bisa dipush juga
-cars.push('Mazda');
-console.log(cars);
-
-// Tuple
-// biasa digunakan di pagination yg nilai balikannya ada lebih dari 1
-let animals: [string, number] = ['Kucing oren', 4];
-animals.push(20);
-console.log(animals);
-console.log(animals[0]);
-
-// Object
-const Employee = {
-    name: 'John',
-    age: 29,
-    skills: ['Excel', 'PPT', 'Word']
-}
-
-// type alias
-// Ngga cuman berlaku di object
-// Employee ini jadi tipe data juga
-type Employee = {
-    name: string,
-    age: number,
-    skills: string[]
-}
-
-const john: Employee = {
-    name: 'John',
-    age: 29,
-    skills: ['Office']
-}
-
-// Function
-function sayHello(name: string, age?:number): string { //tanda tanya ini memberikan nilai optional, bisa diisi atau engga. Kalo ngga ada tanda tanyanya, ini akan error karena function ini meminta nilai masukkan, sedangkan waktu di console.log() kosong dan ngga diisi
-    //narrowing
-    if(typeof age !== undefined){
-        return `Hello ${name}`
-    }
-    return `Hai...`
-}
-
-console.log(sayHello('Daud'));
-
-// Function ini juga berlaku bagi employee
-function sayHello2(employee: Employee): Employee {
-    employee.name = 'John';
-    employee.age = 20;
-    employee.skills = ['Office']
-    return employee
-}
-
-console.log(sayHello2(Employee));
-
-// Import
-import { greeting } from "./greeting";
-
-greeting();
-
 //Udah beda dari yg atas
 import { Car } from "./object/car";
 import { CarType } from "./object/car-type";
-const sedan: Car = new Car();
-sedan.id = '1';
-sedan.name = 'Civic';
-sedan.brand = 'Honda';
-sedan.engine = 2;
-sedan.type = CarType.SEDAN;
-console.log(sedan);
+import { Vehicle } from "./object/vehicle";
 
-console.log(sedan.getName());
-sedan.setName('Civic 2.0');
-console.log(sedan);
+const civic: Vehicle = {
+    id: '1',
+    name: 'Civic',
+    brand: 'Honda',
+    type: CarType.SEDAN,
+    engine: 2
+}
 
-sedan.setFuel(0);
+const sedan: Car = new Car(civic);
+console.log(sedan);
+sedan.setFuel(100);
 sedan.startEngine();
--- src/object/car.ts
+++ src/object/car.ts
@@ -1,49 +1,27 @@
 import { Vehicle } from "./vehicle";
 import { CarType } from "./car-type";
 
-// Kalo pake interface, harus pake implements, ngga bisa pake extends
-// implements bisa lebih dari satu
-export class Car implements Vehicle {
-    id: string;
-    name: string;
-    brand: string = '';
-    type: CarType;
-    engine: number;
-    isStarted?: boolean = false;
+export class Car {
     private fuel: number = 0;
+    readonly color: string;
 
-    constructor(car?: Car) {
-        // Karena optional (?) === undefined
-        // harus dicek dulu
-        if(car){
-            this.id = car.id
-            this.name = car.name
-            this.brand = car.brand
-            this.type = car.type
-            this.engine = car.engine
-        }
-    }
+    // Dependency Injection (DI) - > inject file dari luar ke dalam sebuah class
+    // Fungsi -> kita bisa menggunakan file tersebut
+    // readonly -> memberikan sebuah akses dari luar (public)
+    // tetapi tidak bisa diubah nilainya (value)
+    constructor(private readonly vehicle?: Vehicle) {}
 
     //ketika menggunakan setter, kita bisa membuat validasi di dalamnya
-    setFuel(fuel: number){
+    setFuel(fuel: number): void{
         this.fuel = fuel;
     }
 
     startEngine(): void {
-        this.isStarted = true;
         if(this.fuel > 0) {
             console.log(`Engine car is started... brmmmmm`);
         } else {
             console.log(`Fuel is empty, please fill the tank...`);
         }
         
-    }
-
-    setName(name: string): void {
-        this.name = name
-    }
-
-    getName(): string {
-        return this.name
     }
 }
