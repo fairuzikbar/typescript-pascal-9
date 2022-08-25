344820b666033b24cd41c3a200ae935afc730a20
Fairuz Ikbar Wikantyo <fairuz.ikbar.w@gmail.com>
2022-08-25 17:12:20 +0700

    Observable dasar vs promise

--- src/index.ts
+++ src/index.ts
@@ -18,25 +18,22 @@ class PromiseVersusObservable {
 
     create(): void {
         this.myObservable = new Observable<string>(observer => {
-            console.log('Observable has created.');
-            setInterval(() => {
-                observer.next('Observable has emitted.');
-            },1000)
+            observer.next('Observable has emitted.');
         })
 
         this.myPromise = new Promise<string>(resolve => {
-            console.log('Promise has created.');
-            setInterval(() => {
-                resolve('Promise has emitted.');
-            },1000)
-        })
-    }
-
-    execute(): void {
-        this.mySubscription = this.myObservable.subscribe((data:any) => {
-            console.log(data);  
+            resolve('Promise has emitted.');
         })
 
+        // Observable layaknya synchronous, dia akan dipanggil duluan ketika ada promise yang bersamanya
+        // Bagaimana jika ingin tampil setelah promise?
+        // Caranya dengan menerapkan setTimeOut
+        setTimeout(() => {
+            this.mySubscription = this.myObservable.subscribe((data:any) => {
+                console.log(data);  
+            })
+        }, 0)
+    
         this.myPromise.then((data:any) => {
             console.log(data);  
         })
@@ -48,8 +45,4 @@ class PromiseVersusObservable {
 }
 
 const promiseObservable: PromiseVersusObservable = new PromiseVersusObservable();
-promiseObservable.create();
-promiseObservable.execute();
-setTimeout(() => {
-    promiseObservable.cancel()
-},5000)
+promiseObservable.create()
