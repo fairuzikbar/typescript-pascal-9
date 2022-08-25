a7b566599b4beb593200176a2e4f82063d95e235
Fairuz Ikbar Wikantyo <fairuz.ikbar.w@gmail.com>
2022-08-25 16:50:04 +0700

    Topic - Observable dasar

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,47 @@
-import { Kulkas } from './Kulkas/refrigerator-class'
+// Observable
+/**
+ * - Menyerupai promise
+ * - Setiap obervable, hanya bisa dieksekusi/dijalankan ketika ada yang subscribe
+ * - Yang menjadi subscribe adalah si observer
+ * - Observer ini ada tiga yang bisa digunakan:
+ *   - observer.next (berhasil)
+ *   - observer.error (ketika ada error/gagal)
+ *   - observer.complete (artinya ketika ada lagi observable)
+ * contoh: Try Catch Finally
+ */
 
-const kulkas: Kulkas = new Kulkas();
+import { Observable } from 'rxjs';
+class PromiseVersusObservable {
+    myObservable: any;
+    myPromise: any;
 
-async function cekKulkas() {    
-    let action = await kulkas.buka();
-    console.log(await action.lihat().catch(error => error));
-    console.log(`Sedang menyimpan item...`);
-    console.log(await action.simpan('pepaya').catch(error => error));
-    console.log(await action.simpan('mangga').catch(error => error));
-    console.log(await action.simpan('pisang').catch(error => error));
-    console.log(await action.simpan('jambu').catch(error => error));
-    console.log(await action.lihat().catch(error => error));
-    console.log(await action.ambil('manggis').catch(error => error));
-    console.log(await action.ambil('mangga').catch(error => error));
-    console.log(await action.lihat().catch(error => error));    
-    console.log(await kulkas.tutup());
+    create(): void {
+        this.myObservable = new Observable<string>(observer => {
+            console.log('Observable has created.');
+            observer.next('Observable has emitted 1.');
+            observer.next('Observable has emitted 2.');
+            observer.next('Observable has emitted 3.');
+        })
+
+        this.myPromise = new Promise<string>(resolve => {
+            console.log('Promise has created.');
+            resolve('Promise has emitted 1.');
+            resolve('Promise has emitted 2.');
+            resolve('Promise has emitted 3.');
+        })
+    }
+
+    execute(): void {
+        this.myObservable.subscribe((data:any) => {
+            console.log(data);  
+        })
+
+        this.myPromise.then((data:any) => {
+            console.log(data);  
+        })
+    }
 }
 
-cekKulkas()
+const promiseObservable: PromiseVersusObservable = new PromiseVersusObservable();
+promiseObservable.create();
+promiseObservable.execute();
