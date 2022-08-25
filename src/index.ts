28d09f6a77a3d68fdc3f9fcf3b852ebb892aacf3
Fairuz Ikbar Wikantyo <fairuz.ikbar.w@gmail.com>
2022-08-25 13:02:03 +0700

    Topic - Asynchronous with async await

--- src/index.ts
+++ src/index.ts
@@ -2,65 +2,44 @@ import Cafe from './Asynchronous/order-coffee'
 
 const cafe: Cafe = new Cafe();
 
-// const takeOrder = (error: any, coffee: string) => {
-//     if(error){
-//         console.log(error);
-//     } else {
-//         console.log(coffee);
-//     }
-// }
-
 const orders: string[] = [
     'cafe latte',
     'cappuccino',
     'kopi susu keluarga',
+    'tea',
     'black tea',
     'hazelnut',
-    'ice cream',
-    'tea'
+    'ice cream'
 ];
-// cafe.orderCoffee(orders[6], (error: any, coffee: string) => {
-//     if(error) console.log(error);
-//     else console.log(coffee);    
 
-//     cafe.orderCoffee(orders[5], (error: any, coffee: string) => {
-//         if(error) console.log(error);
-//         else console.log(coffee);
+// const takeOrder: Promise<string>[] = orders.map((order) => {
+//     return cafe.orderCoffeeWithAsync(order)
+// })
 
-//         cafe.orderCoffee(orders[4], (error: any, coffee: string) => {
-//             if(error) console.log(error);
-//             else console.log(coffee);
+// Promise.all(takeOrder)
+// .then((result: string[]) => {
+//     console.log(result)    
+// })
+// .catch((error) => {
+//     console.error(error);
+// })
 
-//             cafe.orderCoffee(orders[3], (error: any, coffee: string) => {
-//                 if(error) console.log(error);
-//                 else console.log(coffee);
+async function orderProcess(): Promise<string[]>{
+    const result: string[] = [];
+    for(const order of orders){
+        try {
+            const takeOrder: string = await cafe.orderCoffeeWithAsync(order);
+            result.push(takeOrder);
+        } catch (error) {
+            console.log(`Pesanan ${order} tidak berhasil, karena ${error}`);
+        }
+    }
+    return result;
+}
 
-//                 cafe.orderCoffee(orders[2], (error: any, coffee: string) => {
-//                     if(error) console.log(error);
-//                     else console.log(coffee);
-
-//                     cafe.orderCoffee(orders[1], (error: any, coffee: string) => {
-//                         if(error) console.log(error);
-//                         else console.log(coffee);
-
-//                         cafe.orderCoffee(orders[0], (error: any, coffee: string) => {
-//                             if(error) console.log(error);
-//                             else console.log(coffee);
-//                         });
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
-
-const takeOrder: Promise<string>[] = orders.map((order) => {
-    return cafe.orderCoffeeWithAsync(order)
-})
-
-Promise.all(takeOrder)
+orderProcess()
 .then((result: string[]) => {
-    console.log(result)    
+    console.log(result);
 })
 .catch((error) => {
     console.error(error);
