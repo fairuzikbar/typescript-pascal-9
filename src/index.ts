7b51d0061111b33bbca44db6511ecd2e156f0aaf
Fairuz Ikbar Wikantyo <fairuz.ikbar.w@gmail.com>
2022-08-25 10:08:36 +0700

    Response Template

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,28 @@
-// Generic
-// Kalo menerapkan generic:
-// Flexible untuk menerima jenis tipe data
-// Keyword generic -> T
-// Penggunaan nya bisa di function, class, interface
+// Template Pagination & Single Response
 
-// Before
-function getData(items: any[]): any[] {
-    return [].concat(items);
+// Interface ini bisa digunakan sebagai tipe data, bisa juga sebagai property
+interface Paging {
+    page: number;
+    rowsPerPage: number;
+    totalRows: number;
+    totalPages: number;
 }
 
-const people: any[] = getData(['John', 'Alex', 'Kohl']);
-const dice: any[] = getData([1, 2, 3, 4, 5, 6]);
-console.log(people);
-console.log(dice);
-
-
-// After Penggunaan Generic
-function getDataOther<T>(items: T[]): T[] {
-    return new Array<T>().concat(items);
+interface ResponseEntity<T> {
+    code: number;
+    message: string;
+    data: T
 }
 
-const people01: Array<string> = getDataOther<string>(['John', 'Alex', 'Kohl']);
-const dice01: Array<number> = getDataOther<number>([1, 2, 3, 4, 5, 6]);
-console.log(people01);
-console.log(dice01);
-
-
-// Penggunaan generic pada type alias
-type Employee = {
-    name: string;
-    age: number;
-    skills?: string[];
+class SingleResponse<T> implements ResponseEntity<T> {
+    code: number;
+    message: string;
+    data: T
 }
 
-const employees: Employee[] = getDataOther<Employee>([
-    {name: 'Daud', age: 20, skills: ['Belajar Menghajar']},
-    {name: 'Fahlan', age: 20},
-    {name: 'Riza', age: 20},
-    {name: 'Aisyah', age: 20}
-])
-
-console.log(employees);
+class PagedResponse<T> implements ResponseEntity<T> {
+    code: number;
+    message: string;
+    data: T;
+    paging: Paging
+}
